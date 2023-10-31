885afd8c3f62884d4be689f9b7e5612ce6ec03bc
miguelnatus <miguelnatus@gmail.com>
2023-10-31 10:48:46 -0300

    css

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from utils.rands import slugify_new
+from django.urls import reverse
 
 class Tag(models.Model):
     class Meta:
@@ -16,3 +17,53 @@ class Tag(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name)
         return super().save(*args, **kwargs)
+    
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True, default=None,
+        null=True, blank=True, max_length=255,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.name)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Page(models.Model):
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True, default="",
+        null=False, blank=True, max_length=255
+    )
+    css = models.TextField()
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisará estar marcado '
+            'para a página ser exibida publicamente.'
+        ),
+    )
+    content = models.TextField()
+
+    def get_absolute_url(self):
+        if not self.is_published:
+            return reverse('blog:index')
+        return reverse('blog:page', args=(self.slug,))
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title,)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
+
